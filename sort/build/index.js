4fb4c4d38a3ee47a35f1e9af6758b04bb0fc99c2
VitalR <vitalii.rodikov@gmail.com>
2022-07-16 13:53:32 +0300

    Add files via upload

---
+++ sort/build/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const LinkedList_1 = require("./LinkedList");
+// const numberCollection = new NumbersCollection([10, 3, -5, 0])
+// numberCollection.sort()
+// console.log(numberCollection.data)
+// const charactersCollection = new CharactersCollection('Xaayb')
+// charactersCollection.sort()
+// console.log(charactersCollection.data)
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(5);
+linkedList.sort();
+linkedList.print();
