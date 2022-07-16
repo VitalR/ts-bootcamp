8d0a30082ec2b31f72c9f68c8cc4c5068fa21178
VitalR <vitalii.rodikov@gmail.com>
2022-07-16 14:02:24 +0300

    Add files via upload

---
+++ rrts/src/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { App } from './components/App'
+import { reducers } from './reducers'
+
+const store = createStore(reducers, applyMiddleware(thunk))
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+)
+
+// const App = (props: AppProps): JSX.Element => {
+//     return <div>{props.color}</div>
+// }
+
+// interface AppProps {
+//     color?: string
+// }
+
+// class App extends React.Component<AppProps> {
+//     state = { counter: 0 }
+    
+//     constructor(props: AppProps) {
+//         super(props)
+//     }
+
+//     onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1 })
+//     }
+
+//     onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1 })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.onIncrement}>Increment</button>
+//                 <button onClick={this.onDecrement}>Decrement</button>
+//                 {this.state.counter}
+//             </div>
+//         )
+//     }
+// }
